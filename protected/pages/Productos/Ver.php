4bcd2683a235f8471ad3e6dc232c8ebdc633301d
algerion <orctek-varios@yahoo.com.mx>
2015-09-14 13:18:12 -0500

    Búsqueda de producto
    Agregada búsqueda de producto por descripción en registro de venta

--- protected/pages/Productos/Ver.php
+++ protected/pages/Productos/Ver.php
@@ -19,6 +19,11 @@ class Ver extends TPage
 		{
 			$resultado = Conexion::Retorna_Registro($this->dbConexion, "productos", array(), 
 					" descripcion LIKE '%" . $this->txtBusqueda->Text . "%'");
+			if(isset($this->Request["tipo"]))
+				if($this->Request["tipo"] == "link")
+					for($i = 0; $i < count($resultado); $i++)
+						$resultado[$i]["codigo"] = "<a href='#' onclick=\"opener.document.getElementById('ctl0_Main_txtCodigo').value = '" . 
+								$resultado[$i]["codigo"] . "'; window.close();\" >" . $resultado[$i]["codigo"] . "</a>";
 			$this->dgProductos->DataSource = $resultado;
 			$this->dgProductos->dataBind();
 		}
