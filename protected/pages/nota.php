09a9ec4cebae011705d24e6c8ad5cfb120ef7b81
Angel Bravo <orctek-varios@yahoo.com.mx>
2015-09-17 02:07:28 -0500

    Descuento y total
    Agregado de descuentos y totales.

--- protected/pages/nota.php
+++ protected/pages/nota.php
@@ -18,8 +18,23 @@ class Nota extends TPage
 		{
 			if(isset($this->Request["nota"]))
 			{
+				$subtotal = Conexion::Retorna_Campo($this->dbConexion, "notas_productos", 
+						"SUM(precio * cantidad)", array("id_nota"=>$this->Request["nota"]));
+				$datos_nota = Conexion::Retorna_Registro($this->dbConexion, "notas", 
+						array("id_nota"=>$this->Request["nota"]));
+				if($datos_nota[0]["vales"] > 0)
+				{
+					$vale = Conexion::Retorna_Campo($this->dbConexion, "parametros", 
+						"valor", array("llave"=>"vale"));
+					$descuento = $vale * $datos_nota[0]["vales"];
+				}
+				else
+					$descuento = $subtotal * $datos_nota[0]["descuento"] / 100;
+
 				$this->lblNota->Text = $this->Request["nota"];
-				$this->lblTotal->Text = Conexion::Retorna_Campo($this->dbConexion, "notas_productos", "SUM(precio * cantidad)", array("id_nota"=>$this->Request["nota"]));
+				$this->lblSubtotal->Text = $subtotal;
+				$this->lblDescuento->Text = $descuento;
+				$this->lblTotal->Text = $subtotal - $descuento;
 				$this->Master->Page->Title = "Nota " . $this->Request["nota"];
 				$consulta = "SELECT p.Codigo, p.Descripcion, np.Cantidad, np.Precio, np.cantidad * np.precio AS Total " . 
 						"FROM productos p JOIN notas_productos np ON p.id_producto = np.id_producto " .
