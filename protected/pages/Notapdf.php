d8fe12415cb5304fa377c80a3f6fdc2e3dd94d29
Angel Bravo <orctek-varios@yahoo.com.mx>
2015-09-16 23:39:57 -0500

    Ajuste de línea
    Recortado de la línea de invocación de usadompdf para mejorar
    visualización

--- protected/pages/Notapdf.php
+++ protected/pages/Notapdf.php
@@ -7,7 +7,9 @@ class Notapdf extends TPage
 	{
 		parent::onLoad($param);
 
-		usadompdf::creapdf("http://" . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"] . "?page=nota&nota=" . $this->Request["nota"], "a5");
+		usadompdf::creapdf("http://" . $_SERVER["HTTP_HOST"] 
+				. $_SERVER["PHP_SELF"] . "?page=nota&nota=" . 
+				$this->Request["nota"], "a5");
 /*		foreach($_SERVER as $k=>$s)
 			echo $k . "=>" . $s . "<br />";*/
 	}
