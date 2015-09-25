7f9f14a1f5d11839fe927cc30fcc52f5e41babbb
algerion <orctek-varios@yahoo.com.mx>
2015-09-25 16:12:57 -0500

    Eliminado código innecesario en scaffold modificar
    Se quitó código de conexión porque el scaffold lo jala del archivo xml

--- protected/pages/Productos/Modificar.php
+++ protected/pages/Productos/Modificar.php
@@ -1,23 +1,8 @@
 <?php
 Prado::using('System.Data.ActiveRecord.Scaffold.*'); 
-//Prado::using('System.Web.UI.ActiveControls.*');
-include_once('../compartidos/clases/conexion.php');
 include_once('Productos.php');
 
 class Modificar extends TPage
 {
-	var $dbConexion;
-
-	public function onLoad($param)
-	{
-		parent::onLoad($param);
-
-		$this->dbConexion = Conexion::getConexion($this->Application, "db");
-		Conexion::createConfiguracion();
-
-		if(!$this->IsPostBack)
-		{
-		}
-	}
 }
 ?>
